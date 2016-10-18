c4ef072cfe34ba791ef4078f0dc859cede95b77f
Omegamean <cg0364@my.bristol.ac.uk>
2016-10-18 08:11:38 +0100

    Added much more to it - mainly splitting out the turn function for a player and AI

--- Naughts_and_Crosses.py
+++ Naughts_and_Crosses.py
@@ -1,8 +1,65 @@
 board_state = [['| |','| |','| |'],['| |','| |','| |'],['| |','| |','| |']]
+win_condition = ()
 def printboard(list):
 	for l in list:
 		print(''.join(l))
+
+def end_game():
+	if board_state == win_condition:
+		print('Well Done, you won! Good Job')
+	else:
+		print('Oh dear, looks like a draw...')
+	print('Would you like to play again?')
+
+
+
+class Player:
+	def __init__(self,name):
+		Player.name = name
+	def choice_row():
+		x=()
+		while x not in range(0,3):
+			x= input('Pick what row you would like') - 1
+			if x not in range(0,3):
+				print('That\'s not in the right format you douche, try again')
+		return x
+
+	def choice_column():
+		y=()
+		while y not in range(0,3):
+			y= input('Pick what column you would like') - 1
+			if y not in range(0,3):
+				print('That\'s not in the right format you douche, try again')
+				y = ()
+		return y
+	def turn():
+		turn = 'On-Going'
+		while turn == 'On-Going':
+			x_row = choice_row()
+			y_column = choice_column()
+			if board_state[x_row][y_column] == '|X|' or board_state[x_row][y_column] == '|O|':
+				print('Square\'s already full idiot') 
+			else:
+				board_state[x_row][y_column] = '|X|'
+				turn = 'Ended'
+		return board_state
+
+class AI:
+	def turn():
+		turn = 'On Going'
+		while turn == 'On Going':
+			x_row = randrange(0,3)
+			y_row = randrange(0,3)
+			if board_state[x_row][y_column] == '|X|' or board_state[x_row][y_column] == '|O|':
+				print('Square\'s already full idiot') 
+			else:
+				board_state[x_row][y_column] = '|O|'
+				turn = 'Ended'
+		return board_state
+
+
 	
+
 
 
 		
@@ -24,10 +81,12 @@ while board_state != win_condition: #While loop that runs throughout the whole f
 	free_square_computer = False    # -> computer turn -> player turn etc until someone has won. Need to add a break for full board
 
 	while free_square_player == False: #This while loop is the players turn
-		choice = input('Where would you like to go (type in the form y,z for the coordinates)?').split(',') #Turns players input into coordinates
-		choice[0] = int(choice[0]) - 1 #these translate the coordinates to 'Computer speak'
-		choice[1] = int(choice[1]) - 1
-		if board_state[choice[0][choice[1]]] != '| |': #THIS IS WHERE THE FUNCTION STOPS WORKING. I want to change the board_state[i[j]] into an X
+		choice_x = input('Where row would you like to go?')
+		while choice_x not in range(1,4):
+				 #Turns players input into coordinates
+			choice[0] = int(choice[0]) - 1 #these translate the coordinates to 'Computer speak'
+			choice[1] = int(choice[1]) - 1
+		if board_state[choice[0]][choice[1]] != '| |': #THIS IS WHERE THE FUNCTION STOPS WORKING. I want to change the board_state[i[j]] into an X
 			print('That is not a valid choice, soz')
 		else:
 			board_state[int(choice[0])[int(choice[1])]] = '|X|'
