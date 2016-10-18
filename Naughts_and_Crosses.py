dbc71797cfabfd876ce4fa3cb07202e2525d67bf
Omegamean <cg0364@my.bristol.ac.uk>
2016-10-18 22:20:17 +0100

    Toms changes mainly

--- Naughts_and_Crosses.py
+++ Naughts_and_Crosses.py
@@ -1,60 +1,70 @@
-board_state = [['| |','| |','| |'],['| |','| |','| |'],['| |','| |','| |']]
+board_state = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 win_condition = ()
+def check_win(board_state):
+	if board_state is in win_condition:
+		return True
+	else:
+		return False
+
+def check_draw():
+
+
 def printboard(list):
 	for l in list:
-		print(''.join(l))
+		print('|'.join(l))
 
 def end_game():
-	if board_state == win_condition:
+	if board_state is in win_condition:
 		print('Well Done, you won! Good Job')
+		print('Would you like to play again?')
+		return True
 	else:
-		print('Oh dear, looks like a draw...')
-	print('Would you like to play again?')
+		return False
 
 
 
 class Player:
 	def __init__(self,name):
-		Player.name = name
+		self.name = input('What is your name?')
 	def choice_row():
 		x=()
-		while x not in range(0,3):
-			x= input('Pick what row you would like') - 1
-			if x not in range(0,3):
+		while x not in range(3):
+			x= int(input('Pick what row you would like') - 1)
+			if x not in range(3):
 				print('That\'s not in the right format you douche, try again')
 		return x
 
 	def choice_column():
 		y=()
-		while y not in range(0,3):
-			y= input('Pick what column you would like') - 1
-			if y not in range(0,3):
+		while y not in range(3):
+			y= int(input('Pick what column you would like') - 1)
+			if y not in range(3):
 				print('That\'s not in the right format you douche, try again')
 				y = ()
 		return y
 	def turn():
-		turn = 'On-Going'
-		while turn == 'On-Going':
+		turn = True
+		while turn:
 			x_row = choice_row()
 			y_column = choice_column()
-			if board_state[x_row][y_column] == '|X|' or board_state[x_row][y_column] == '|O|':
+			if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
 				print('Square\'s already full idiot') 
 			else:
-				board_state[x_row][y_column] = '|X|'
-				turn = 'Ended'
+				board_state[x_row][y_column] = 'X'
+				turn = False
 		return board_state
 
 class AI:
 	def turn():
-		turn = 'On Going'
-		while turn == 'On Going':
+		turn = True
+		while turn:
 			x_row = randrange(0,3)
 			y_row = randrange(0,3)
-			if board_state[x_row][y_column] == '|X|' or board_state[x_row][y_column] == '|O|':
+			if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
 				print('Square\'s already full idiot') 
 			else:
-				board_state[x_row][y_column] = '|O|'
-				turn = 'Ended'
+				board_state[x_row][y_column] = 'O'
+				turn = False
 		return board_state
 
 
@@ -86,10 +96,10 @@ while board_state != win_condition: #While loop that runs throughout the whole f
 				 #Turns players input into coordinates
 			choice[0] = int(choice[0]) - 1 #these translate the coordinates to 'Computer speak'
 			choice[1] = int(choice[1]) - 1
-		if board_state[choice[0]][choice[1]] != '| |': #THIS IS WHERE THE FUNCTION STOPS WORKING. I want to change the board_state[i[j]] into an X
+		if board_state[choice[0]][choice[1]] != ' ': #THIS IS WHERE THE FUNCTION STOPS WORKING. I want to change the board_state[i[j]] into an X
 			print('That is not a valid choice, soz')
 		else:
-			board_state[int(choice[0])[int(choice[1])]] = '|X|'
+			board_state[int(choice[0])[int(choice[1])]] = 'X'
 			free_square = True
 		printboard(board_state)
 	if board_state == win_condition:
@@ -98,8 +108,8 @@ while board_state != win_condition: #While loop that runs throughout the whole f
 		while free_square_computer == False: #While loop for the computer's turn but randomly generates numbers
 			x_coordinate = randrange(0,3)
 			y_coordinate = randrage(0,3)
-			if board_state[x_coordinate[y_coordinate]] == '| |':
-				board_state[x_coordinate[y_coordinate]] = '|O|'
+			if board_state[x_coordinate[y_coordinate]] == ' ':
+				board_state[x_coordinate[y_coordinate]] = 'O'
 				free_square_computer = True
 		printboard(board_state)
 	if board_state == win_condition:
