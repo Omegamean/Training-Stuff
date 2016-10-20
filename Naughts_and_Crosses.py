35fd360a9e69a25f06ca2ab8f1f05cfa0bb85d93
Omegamean <cg0364@my.bristol.ac.uk>
2016-10-20 08:03:59 +0100

    Started to get Game class in order, wrote win conditions

--- Naughts_and_Crosses.py
+++ Naughts_and_Crosses.py
@@ -1,21 +1,39 @@
 board_state_start = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 win_condition = ()
 board_state = board_state_start
+s = None
 
+win_condition = [
+	#Horizontal Wins
+	[board_state[0][0], board_state[0][1], board_state[0][2]],
+	[board_state[1][0], board_state[1][1], board_state[1][2]],
+	[board_state[2][0], board_state[2][1], board_state[2][2]],
 
-def start_game(s):
-    while s != 'y':
+	#Vertical Wins
+	[board_state[0][0], board_state[1][0], board_state[2][0]],
+	[board_state[0][1], board_state[1][1], board_state[2][1]],
+	[board_state[0][2], board_state[1][2], board_state[2][2]],
+
+	#TR BL
+	[board_state[0][0], board_state[1][1], board_state[2][2]],
+
+	#TL BR
+	[board_state[0][2], board_state[1][1], board_state[2][0]]
+]
+
+class Game():
+    def start_game(self):
+	    s = input('Would you like to play a game? (y/n)  ')
         if s == 'n':
             print('Okay fine then...')
-            s = input('Come ooooooon, please? (y/n)')
+            start_game(self)
         elif s == 'y':
-            name = input('What is your name?')
             board_state = board_state_start
+            start_game(self)
         else:
             print('I cannot do that Hal...')
-            s = input('Try again using only (y/n)')
-
-
+            print('Try again using only (y/n)')
+           start_game(self)
 
 def check_win(board_state):
     if board_state in win_condition:
@@ -25,8 +43,7 @@ def check_win(board_state):
 
 def check_draw():
     if ' ' not in board_state:
-        print('Oh dear, looks like there\' no more moves left!')
-        would_you_like_to_play = input('Would you like to play again? (y/n)')
+        print('Oh dear, looks like there\'s no more moves left!')
         start_game(would_you_like_to_play)
 
 
@@ -42,9 +59,11 @@ def end_game():
 
 
 class Player1:
-    def __init__(self,name,symbol):
+    def __init__(self,name, symbol):
         self.name = input('What is your name?')
-        self.symobl = input('Which symbol would you like? (X/O)')
+        self.symbol = 'X'
+        # self.symbol = input('Which symbol would you like? (X/O)')
+
     def choice_row():
         x=()
         while x not in range(3):
@@ -61,31 +80,28 @@ class Player1:
                 print('That\'s not in the right format you douche, try again')
                 y = ()
         return y
-    def turn():
-        turn = True
-        while turn:
-            x_row = choice_row()
-            y_column = choice_column()
-            if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
-                print('Square\'s already full idiot') 
-            else:
-                board_state[x_row][y_column] = 'X'
-                turn = False
 
-        return board_state
+    def turn(self):
+        x_row = choice_row()
+        y_column = choice_column()
+        if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
+            print('Square\'s already full idiot') 
+            turn(self)
+        else:
+            board_state[x_row][y_column] = self.symbol
+            return board_state
 
-class AI:
-    def turn():
-        turn = True
-        while turn:
-            x_row = randrange(3)
-            y_column = randrange(3)
-            if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
-                turn = True
-            else:
-                board_state[x_row][y_column] = 'O'
-                turn = False
-        return board_state
+class AI():
+	def __init__(self, symbol):
+		self.symbol = 'O'
+    def turn(self):
+        x_row = randrange(3)
+        y_column = randrange(3)
+        if board_state[x_row][y_column] == 'X' or board_state[x_row][y_column] == 'O':
+            turn(self)
+        else:
+            board_state[x_row][y_column] = self.symbol
+            return board_state
 
 
 
@@ -96,7 +112,7 @@ class AI:
   #Check win condition #
   #Repeat until win condition is in there or board is full #
   #Ask if they would like to play again, then ask if they would like to player against a second player or AI again #
-    
+  
 
 
 
